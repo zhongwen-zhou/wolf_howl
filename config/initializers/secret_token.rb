79a54af2ce52fe9035fbff9808bc191245bc5da1
tony <tony@ubuntu.(none)>
2013-01-24 14:14:00 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WolfHowl::Application.config.secret_token = '3a1312e36442bd4e2b10dc3909427f132336917723f60868decb35985cfb84a79324e9eceae181d6eecd6fe3568fd04dc3ff2db7ac1935d2dbc0d3382ffd3d4f'
